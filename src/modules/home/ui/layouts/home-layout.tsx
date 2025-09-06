375f8d25718f91750f251ad0b93aba79f0c39fe3
Tobias Teles <tobias.teles@gmail.com>
2025-09-06 04:10:02 -0300

    Layout basico

--- src/modules/home/ui/layouts/home-layout.tsx
+++ src/modules/home/ui/layouts/home-layout.tsx
@@ -1,3 +1,6 @@
+import { SidebarProvider } from "@/components/ui/sidebar";
+import { HomeNavebar } from "../components/home-navbar";
+
 interface HomeLayoutProps {
     children: React.ReactNode;
 
@@ -5,12 +8,13 @@ interface HomeLayoutProps {
 
 export const HomeLayout = ({ children }: HomeLayoutProps) => {
     return (
-        <div>
-            <div>
-                <p>Home Navbar</p>
+        <SidebarProvider>
+            <div className="w-full">
+                <HomeNavebar />
+            <div className="flex min-h-screen pt-[4rem]">{children}</div>
             </div>
-            {children}
-        </div>
+            
+        </SidebarProvider>
     )
 }
 
