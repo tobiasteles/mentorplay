b4a13f9bc3ced240a11bc7915a2347e51dd2ab7a
Tobias Teles <tobias.teles@gmail.com>
2025-09-06 22:52:46 -0300

    Layout basico

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,18 +1,10 @@
-import Image from "next/image";
+
 
 
 export default function Home() {
   return (
     <div>
-      <Image
-      src="/logo.svg"
-      height={50}
-      width={50}
-      alt="Logo"
-      />
-      <p className="text-lg font-semibold tracking-tight">
-        MentorPlay2
-      </p>
+     Eu vou carregar videos no futuro
     </div>
   )
 }
--- src/modules/home/ui/layouts/home-layout.tsx
+++ src/modules/home/ui/layouts/home-layout.tsx
@@ -1,20 +1,23 @@
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { HomeNavebar } from "../components/home-navbar";
+import { HomeSidebar } from "../components/home-sidebar";
 
 interface HomeLayoutProps {
-    children: React.ReactNode;
-
+  children: React.ReactNode;
 }
 
 export const HomeLayout = ({ children }: HomeLayoutProps) => {
-    return (
-        <SidebarProvider>
-            <div className="w-full">
-                <HomeNavebar />
-            <div className="flex min-h-screen pt-[4rem]">{children}</div>
+  return (
+    <SidebarProvider>
+      <div className="w-full">
+        <HomeNavebar />
+        <div className="flex min-h-screen pt-[4rem]">
+            <HomeSidebar />
+            <main className="flex-1 overflow-y-auto">
+            {children}
+            </main>
             </div>
-            
-        </SidebarProvider>
-    )
-}
-
+      </div>
+    </SidebarProvider>
+  );
+};
