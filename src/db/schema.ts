f7e99f36d55d79bd736a57e745e3cf1e9bf1d3b7
Tobias Teles <tobias.teles@gmail.com>
2025-09-13 18:54:17 -0300

    configurei o database

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,4 @@
-import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import { pgTable, text, timestamp, uniqueIndex, uuid } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
     id: uuid("id").primaryKey().defaultRandom(),
@@ -8,4 +8,5 @@ export const users = pgTable("users", {
     imageUrl: text("image_url").notNull(),
     createdAt: timestamp("created_at").defaultNow().notNull(),
     updatedAt: timestamp("updated_at").defaultNow().notNull(),
-})
+}, (t) => [uniqueIndex("clerk_id_idx").on(t.clerkId)]);
+
